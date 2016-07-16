5dcd0fd89941a847779a7e21ff8886537ca468d1
AbbinVarghese <paadam2@gmail.com>
2016-07-17 00:54:40 +0530

    Initial Commit

---
+++ TheFoodProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFoodProject
+//
+//  Created by Abbin Varghese on 17/07/16.
+//  Copyright © 2016 TheFoodCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheFoodProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheFoodProject
+//
+//  Created by Abbin Varghese on 17/07/16.
+//  Copyright © 2016 TheFoodCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
